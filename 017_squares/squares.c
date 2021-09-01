2a90289bd8a2b72980330983379fea84defa96b5
ogg4 <ogg4@duke.edu>
2021-09-01 14:24:25 -0400

    Assignment 017, squares

--- 017_squares/squares.c
+++ 017_squares/squares.c
@@ -1,12 +1,27 @@
+int max (int size1, int size2, int offset);
+_Bool between(int pos, int offset, int size);
+_Bool equal(int pos, int offset, int size);
+_Bool lessThan(int pos1, int pos2,  int size);
+
 void squares(int size1, int x_offset, int y_offset, int size2) {
   //compute the max of size1 and (x_offset + size2).  Call this w
-
+  int w = max(size1, size2, x_offset);
   //compute the max of size1 and (y_offset + size2).  Call this h
-
+  int h = max(size1, size2, y_offset);
   //count from 0 to h. Call the number you count with y
-
+  for (int y = 0; y < h; y++) {
     //count from 0 to w. Call the number you count with x
-
+    for (int x = 0; x < w; x++) {
+      if ((between(x, x_offset, size2) && equal(y, y_offset, size2)) || (between(y, y_offset, size2) && equal(x, x_offset, size2))) {
+        printf("*");
+      } else if (lessThan(x, y, size1) || lessThan(y, x, size1)) {
+        printf("#");
+      } else {
+        printf(" ");
+      }
+    }
+    printf("\n");
+  }
       //check if  EITHER
       //    ((x is between x_offset  and x_offset +size2) AND 
       //     y is equal to either y_offset OR y_offset + size2 - 1 )
@@ -26,4 +41,19 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
     //when you finish counting x from 0 to w, 
     //print a newline
 
+}
+int max (int size1, int size2, int offset) {
+  if (size1 > (size2 + offset)) {
+    return size1;
+  }
+  return size2 + offset;
+}
+_Bool between (int pos, int offset, int size){
+  return pos >= offset && pos < (offset + size);
+}
+_Bool equal (int pos, int offset, int size) {
+  return pos == offset || pos == (offset + size - 1);
+}
+_Bool lessThan (int pos1, int pos2,  int size) {
+  return pos1 < size && (pos2 == 0 || pos2 == size - 1);
 }
