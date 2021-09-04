0415cc0da01cf9c3f4784299b479d3165eb96612
ogg4 <ogg4@duke.edu>
2021-09-04 13:31:52 -0400

    Assignmetn 021, rectangle

--- 021_rectangle/rectangle.c
+++ 021_rectangle/rectangle.c
@@ -16,15 +16,48 @@ int max (int a, int b) {
 }
 
 //Declare your rectangle structure here!
+struct _rectangle {
+  int x;
+  int y;
+  int width;
+  int height;
+};
+typedef struct _rectangle rectangle;
 
 
 rectangle canonicalize(rectangle r) {
   //WRITE THIS FUNCTION
+  if (r.width < 0) {
+    r.x += r.width;
+    r.width *= -1;
+  }
+  if (r.height < 0) {
+    r.y += r.height;
+    r.height *= -1;
+  }
   return r;
 }
 rectangle intersection(rectangle r1, rectangle r2) {
   //WRITE THIS FUNCTION
-  return r1;
+  r1 = canonicalize (r1);
+  r2 = canonicalize (r2);
+  rectangle rAns;
+
+  rAns.x = max (r1.x, r2.x);
+  rAns.y = max (r1.y, r2.y);
+
+  int rBounds = min (r1.x + r1.width, r2.x + r2.width);
+  int topBounds = min (r1.y + r1.height, r2.y + r2.height);
+
+  rAns.width = rBounds - rAns.x;
+  rAns.height = topBounds - rAns.y;
+
+  if (rAns.width < 0 || rAns.height < 0) {
+    rAns.width = 0;
+    rAns.height = 0;
+  }
+
+  return rAns;
 }
 
 //You should not need to modify any code below this line
