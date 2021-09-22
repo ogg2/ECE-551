8e0f8185d062dab4748d7892e2de8164bab815c3
ogg4 <ogg4@duke.edu>
2021-09-22 14:50:58 -0400

    Assignment 47, break Caesar Cipher

---
+++ 047_break_encr/breaker.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int breaker (int * freq) {
+  int freqCount = freq[0];
+  int keyCode = 0;
+  for (int i = 1; i < 26; i++) {
+    if (freq[i] > freqCount) {
+      freqCount = freq[i];
+      keyCode = i;
+    }
+  }
+  keyCode = keyCode + 'a' - 'e';
+  keyCode += 26;
+  keyCode %= 26;
+
+  return keyCode;
+}
+
+void frequency (FILE * file, int * freq) {
+  int c;
+  while ((c = fgetc(file)) != EOF) {
+    if (isalpha(c)){
+      c = tolower (c);
+      c -= 'a';
+      //int count = freq[c];
+      //freq[c] = count++;
+      freq[c]++;
+    }
+  }
+}
+
+int main (int argc, char ** argv) {
+  if (argc != 1) {
+    fprintf(stderr, "Usage: inputFileName\n");
+    EXIT_FAILURE;
+  }
+
+  FILE * file = fopen (argv[1], "r");
+  if (file == NULL) {
+    perror ("Could not open file");
+    return EXIT_FAILURE;
+  }
+  int freq[26] = {0};
+  frequency(file, freq);
+  
+  int codeKey = breaker (freq);
+  
+  if (fclose(file) != 0) {
+    perror ("Failed to clsoe the input file!");
+    return EXIT_FAILURE;
+  }
+
+  printf("%d\n", codeKey);
+
+  return EXIT_SUCCESS;
+}
