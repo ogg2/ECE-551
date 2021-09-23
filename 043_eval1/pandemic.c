4d6e7fcca445741c4d24295517b1aa6e598b6a5a
ogg4 <ogg4@duke.edu>
2021-09-23 17:05:22 -0400

    printCountries, Step 1 testing

--- 043_eval1/pandemic.c
+++ 043_eval1/pandemic.c
@@ -1,10 +1,39 @@
 #include "pandemic.h"
+#include <stdio.h>
+#include <ctype.h>
 
+/**
+* parseLine takes an input string that contains a country name and population
+* and inputs it into a struct of type country_t 
+* input: line - string from an input file containing country name and population
+*
+* return: country_t - struct containing the country name and population 
+**/
 country_t parseLine(char * line) {
   //WRITE ME
   country_t ans;
-  ans.name[0] = '\0';
-  ans.population = 0;
+  int i = 0;
+  char character;
+  while ((character = *line) != ',') {
+    if (i > 62) {
+      fprintf (stderr, "Error: Country name too long.\n");
+      exit (EXIT_FAILURE);
+    }
+    //if (isalpha(character) || character == ' ') {
+      ans.name[i] = character;
+      line++;
+      i++;
+    //}
+  }
+  ans.name[i] = '\0';
+  line++;
+  uint64_t population = atoi (line);
+  if (population == 0) {
+    fprintf (stderr, "Error: Population count for %s is not a number.\n", ans.name);
+    exit (EXIT_FAILURE);
+  }
+  ans.population = population;
+
   return ans;
 }
 
