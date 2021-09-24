b41e924446d6ca8f2ec6f516afe7b5c8cbb7b7c4
ogg4 <ogg4@duke.edu>
2021-09-24 19:23:44 -0400

    Added more error checking

--- 049_rot_matrix/rotateMatrix.c
+++ 049_rot_matrix/rotateMatrix.c
@@ -29,28 +29,6 @@ int main (int argc, char ** argv) {
     return EXIT_FAILURE;
   }
 
-  /*char word0[12];
-  fgets(word0, 12, file);
-  char word1[12];
-  fgets(word1, 12, file);
-  char word2[12];
-  fgets(word2, 12, file);
-  char word3[12];
-  fgets(word3, 12, file);
-  char word4[12];
-  fgets(word4, 12, file);
-  char word5[12];
-  fgets(word5, 12, file);
-  char word6[12];
-  fgets(word6, 12, file);
-  char word7[12];
-  fgets(word7, 12, file);
-  char word8[12];
-  fgets(word8, 12, file);
-  char word9[12];
-  fgets(word9, 12, file);
-  */
-
   //const char * matrix [12] = {word0, word1, word2, word3, word4, word5, word6, word7, word8, word9};
   const char * matrix[12];
   int i = 0;
@@ -59,14 +37,22 @@ int main (int argc, char ** argv) {
   //WHY DOESNT THIS WORK
   while (fgets(word, 12, file) != NULL) {
     if (strchr(word, '\n') == NULL) {
-      fprintf(stderr, "Line is too long.\n");
+      fprintf(stderr, "Input file is not formatted correctly. Line is too long.\n");
       return EXIT_FAILURE;
     }
+    /*if (word[10] == '\0') {
+      fprintf(stderr, "Input file is not formatted correctly. Line is too short.\n");
+      return EXIT_FAILURE;
+    }*/
     if (i > 9) {
       fprintf(stderr, "Input file is not formatted correctly. Too many lines.\n");
       return EXIT_FAILURE;
     }
     matrix[i++] = strdup(word);
+  }
+  if (i < 9) {
+    fprintf(stderr, "Input file is not formatted correctly. Too few lines.\n");
+    return EXIT_FAILURE;
   }
   
   rotate (matrix);
