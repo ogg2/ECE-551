9fa7ba102984ce4e216fdd63181c7dd89e075d22
ogg4 <ogg4@duke.edu>
2021-09-26 21:54:04 -0400

    Cumulative debugging

--- 043_eval1/pandemic.c
+++ 043_eval1/pandemic.c
@@ -119,10 +119,11 @@ void calcCumulative(unsigned * data, size_t n_days, uint64_t pop, double * cum) 
   }
 
   uint64_t cumulativeCases = 0;
+  double ratio = pop / 100000;
 
   for (size_t day = 0; day < n_days; day++) {
     cumulativeCases += *data;
-    *cum = cumulativeCases / ((double) (pop / 100000));
+    *cum = cumulativeCases / ratio;
 //    printf ("Cumulative Cases: %zu\n", cumulativeCases);
     cum++;
     data++;
