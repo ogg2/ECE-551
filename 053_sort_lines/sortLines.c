1d4dcc27121d5c6f28bfba0868ade5224342f41c
ogg4 <ogg4@duke.edu>
2021-09-28 10:06:55 -0400

    Assignment 053, sorting

--- 053_sort_lines/sortLines.c
+++ 053_sort_lines/sortLines.c
@@ -14,8 +14,55 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
+void printArray (char ** data, size_t size) {
+  //printf("Inside printArray\n");
+  /*while (*data != NULL) {
+    printf ("%s", *data);
+    data++;
+  }*/
+  for (size_t i = 0; i < size; i++) {
+    printf ("%s", data[i]);
+  }
+}
+
+void readFile (FILE * file) {
+  char * line = NULL;
+  size_t sz;
+  char ** data = NULL;
+  size_t numLines = 0;
+  while (getline (&line, &sz, file) >= 0) {
+    data = realloc (data, (1 + numLines) * sizeof (*data));  
+    data[numLines] = line;
+    //printf ("Line: %s", data[numLines]);
+    numLines++;
+    line = NULL;
+  }
+  free (line);
+  //printf("First line: %s\n", data[0]);
+  sortData (data, numLines);
+  printArray (data, numLines);
+  free (data);
+}
+
 int main(int argc, char ** argv) {
   //WRITE YOUR CODE HERE!
+  if (argc == 1) {
+    ;
+  }
+  if (argc > 1) {
+    for (int i = 1; i < argc; i++) {
+      FILE * file = fopen (argv[i], "r");
+      if (file == NULL) {
+        fprintf(stderr, "Error: Could not open file.\n");
+        return EXIT_FAILURE;
+      }
+      readFile (file);
+      if (fclose(file) != 0) {
+        fprintf(stderr, "Error: Failed to close the input file!\n");
+        return EXIT_FAILURE;
+      }
+    }
+  }
 
   return EXIT_SUCCESS;
 }
