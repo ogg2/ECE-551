4b377f47339d2308df4d004889ba21ffd9fb9f2d
ogg4 <ogg4@duke.edu>
2021-10-06 21:55:55 -0400

    Assignment 057, output file name

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -5,4 +5,21 @@
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  char * outputName;
+
+  outputName = malloc (sizeof(inputName) + sizeof(inputName) * 6);
+  char * output = outputName;
+  while (*inputName != '\0') {
+    *outputName = *inputName;
+    inputName++;
+    outputName++;
+  }
+  char * counts = ".counts";
+  for (int i = 0; i < 7; i++) {
+    *outputName = counts[i];
+    outputName++;
+  }
+  *outputName = '\0';
+
+  return output;
 }
