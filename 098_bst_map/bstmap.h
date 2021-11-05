c718b008fab3b4deb6bc1d04a346556ce9b97412
ogg4 <ogg4@duke.edu>
2021-11-05 10:36:39 -0400

    Assignment 98, BST class

---
+++ 098_bst_map/bstmap.h
@@ -0,0 +1,122 @@
+#ifndef __BSTMAP_H__
+#define __BSTMAP_H__
+#include <stdexcept>
+#include "map.h"
+
+template<typename K, typename V>
+class BSTMap : public Map<K,V> {
+  public:
+    class Node {
+      public:
+        K key;
+        V value;
+        Node * left;
+        Node * right;
+        Node(K k, V v) : key(k), value(v), left(NULL), right(NULL){};
+    };
+    Node * root;
+
+public:
+  BSTMap () : root(NULL) {}
+
+  virtual void add(const K & key, const V & value) {
+    root = add (root, key, value);
+  }
+
+  virtual Node * add (Node * curr, const K & key, const V & value) {
+    if (curr == NULL) {
+      Node * newNode = new Node (key, value);
+      return newNode;
+    } else {
+      if (key == curr->key) {
+        curr->value = value;
+      } else if (key < curr->key) {
+        Node * newLeft = add (curr->left, key, value);
+        curr->left = newLeft;
+      } else {
+        Node * newRight = add (curr->right, key, value);
+        curr->right = newRight;
+      }
+    }
+    return curr;
+  }
+
+  virtual const V & lookup(const K& key) const throw (std::invalid_argument) {
+    Node * curr = root;
+    while (curr != NULL) {
+      if (curr->key == key) {
+        return curr->value;
+      } else if (key < curr->key) {
+        curr = curr->left;
+      } else {
+        curr = curr->right;
+      }
+    }
+    try {
+      return curr->value;
+    } catch (std::invalid_argument & e) {
+      std::cerr << e.what();
+    }
+  }
+  virtual Node * remove (Node * curr, const K& key) {
+    if (curr == NULL) {
+      return curr;
+    }
+    if (curr->key == key) {
+      ; //IS THIS RIGHT
+    } else {
+      if (key < curr->key) {
+        Node * parentL = remove (curr->left, key);
+        curr->left = parentL;
+      } else {
+        Node * parentR = remove (curr->right, key);
+        curr->right = parentR;
+      }
+      return curr;
+    }
+
+    if (curr->left == NULL) {
+      Node * temp = curr->right;
+      delete curr;
+      return temp;
+    } else if (curr->right == NULL) {
+      Node * temp = curr->left;
+      delete curr;
+      return temp;
+    } else {
+      //NOT REMOVING NODE WITH 2 CHILDREN CORRECTLY
+      //NEED TO UPDATE REPLACEMENT NODE'S ORIGINAL PARENT TO POINT TO NULL
+      Node * temp = curr;
+      curr = curr->left;
+      while (curr != NULL) {
+        curr = curr->right;
+      }
+      curr->left = temp->left;
+      curr->right = temp->right;
+      //NEED TO RETURN EVERYTIME WE CALL A NEW NODE
+      //MAY NEED TO UPDATE CODE ABOVE
+      return temp;
+    }
+  }
+  virtual void remove(const K& key) {
+    /*Node * curr = root;
+    while (curr != NULL) {
+      if (curr->key == key) {
+        Node * ans = remove (curr, key);
+      }
+    }*/
+    root = remove (root, key);
+  }
+  virtual ~BSTMap<K,V>() {
+    destroy (root);
+  }
+
+  void destroy (Node * curr) {
+    if (curr != NULL) {
+      destroy (curr->left);
+      destroy (curr->right);
+      delete curr;
+    }
+  }
+};
+#endif
---
+++ 098_bst_map/test_map.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <assert.h>
+#include <cstdio>
+#include <cstdlib>
+#include "bstmap.h"
+#include <string>
+
+int main (void) {
+  BSTMap<int, std::string> map;
+
+  assert (map.root == NULL);
+  map.add (5, "five");
+  assert (map.root->key == 5);
+  assert (map.root->value == "five");
+  map.add (3, "three");
+  map.add (9, "nine");
+  assert (map.root->left->key == 3);
+  assert (map.root->right->key == 9);
+  map.add (4, "four");
+  std::cout << "Key: " << map.root->key << std::endl;
+  std::cout << "Key: " << map.root->left->key << std::endl;
+  assert (map.root->left->right->key == 4);
+  map.lookup(2); 
+  map.lookup(10); 
+  map.lookup(8); 
+  map.add (3, "thirty");
+  assert (map.lookup(3) == "thirty");
+  assert (map.lookup(4) == "four");
+  assert (map.lookup(5) == "five");
+  assert (map.lookup(9) == "nine");
+
+  map.remove(9);
+  assert (map.root->right == NULL);
+  map.add (9, "nine");
+  assert (map.root->right->key == 9);
+  map.remove(4);
+  assert (map.root->left->right == NULL);
+  map.add (4, "four");
+  assert (map.lookup(4) == "four");
+
+  map.remove(8);
+  assert (map.root->left->key == 3);
+  map.remove(3);
+  assert (map.root->left->key == 4);
+  assert (map.root->left->left == NULL);
+  assert (map.root->left->right == NULL);
+
+  map.remove (4);
+  map.add (3, "three");
+  map.add (4, "four");
+  map.add (0, "zero");
+  map.add (2, "two");
+  map.add (-1, "-one");
+  map.remove (3);
+  assert (map.root->left->key == 2);
+
+  return EXIT_SUCCESS;
+}
