31931157e06a66cd1b49e83f1e8f47739851b7ce
ogg4 <ogg4@duke.edu>
2021-11-08 15:01:58 -0500

    Page file

---
+++ 093_eval3/page.hpp
@@ -0,0 +1,19 @@
+#ifndef __PAGE_HPP__
+#define __PAGE_HPP__
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Page {
+  private:
+    std::string pageText;
+    vector<std::string> choices; 
+  public:
+
+};
+
+
+
+#endif
