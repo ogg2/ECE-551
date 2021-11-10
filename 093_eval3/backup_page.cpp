76d2f428042f2f731433d75baa9d8228a8e5a8e5
ogg4 <ogg4@duke.edu>
2021-11-10 18:31:44 -0500

    Reorganzing step1

--- 093_eval3/backup_page.cpp
+++ 093_eval3/backup_page.cpp
@@ -14,12 +14,11 @@ Page::Page (char * fileName) {
   if (!file) {
     Error ("Could not open file!");
   }
-  file >> *this;
+  file >> choices;
   file.close ();
-//  std::cout << *this;
 }
 
-std::istream & operator>>(std::istream & s, Page & page) {
+std::istream & operator>>(std::istream & s, std::vector<std::pair<std::string, int> > & data) {
   std::string line;
   while (std::getline (s, line)) {
     if (line.substr(0,1).compare("#") != 0) {
@@ -28,43 +27,43 @@ std::istream & operator>>(std::istream & s, Page & page) {
       int nextPage = 0;
       //Handle win/lose in here? Or further checks if line without page number
       //Navigation section error checking
-      if ((colon != std::string::npos || page.choices.size() > 0) && 
+      if ((colon != std::string::npos || data.size() > 0) && 
          ((nextPage = strtol (line.substr(0, colon).c_str(), &extraNumbers, 10)) <= 0 || 
          *extraNumbers != '\0')) {
         Error ("Page number must be integer greater than 0.");
       }
       std::pair<std::string, int> pair (line.substr (colon + 1, line.length()), nextPage);
-      page.choices.push_back (pair);
+      data.push_back (pair);
     } else {
       break;
     }
   }
 
-  if (page.choices.size() == 0) {
+  if (data.size() == 0) {
     Error ("No navigation choices.");
   }
 
   while (std::getline (s, line)) {
-    page.pageText << line << "\n";
+    std::cout << line << std::endl;
   }
-  page.pageText << "\n";
+  std::cout << std::endl;
   return s;
 }
 
-std::ostream & operator<<(std::ostream & s, Page & page) {
-  std::vector<std::pair<std::string, int> >::iterator it = page.choices.begin();
+std::ostream & operator<<(std::ostream & s, std::vector<std::pair<std::string, int> > & data) {
+  std::vector<std::pair<std::string, int> >::iterator it = data.begin();
   int index = 1;
   if (it->second == 0) {
-    if (it->first.compare ("WIN") == 0 && page.choices.size() == 1) {
+    if (it->first.compare ("WIN") == 0 && data.size() == 1) {
       s << "Congratulations! You have won. Hooray!" << std::endl;
-    } else if (it->first.compare ("LOSE") == 0 && page.choices.size() == 1) {
+    } else if (it->first.compare ("LOSE") == 0 && data.size() == 1) {
       s << "Sorry, you have lost. Better luck next time!" << std::endl;
     } else {
       Error ("Navigation choices must include page number.");
     }  
   } else {
       s << "What would you like to do?\n" << std::endl;
-    while (it != page.choices.end()) {
+    while (it != data.end()) {
       s << " " << index << ". " << it->first << std::endl;
       //s << "\t" << it->second << std::endl; //FOR TESTING - DELETE
       ++it;
--- 093_eval3/page.hpp
+++ 093_eval3/page.hpp
@@ -5,20 +5,28 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <sstream>
 
+/**
+* Page is a class that holds the contents of an input page
+*/
 class Page {
   private:
-    std::string pageText;
+    std::stringstream pageText;
+    //pair in vector contains navigation choices in string and next page in 'int'
     std::vector<std::pair<std::string, int> > choices; 
-    //std::vector<int> nextPage;
   public:
     Page (char * fileName);
-
+    void printPage () {
+      std::cout << pageText.str();
+      std::cout << *this;
+    }
+    friend std::istream & operator>>(std::istream & s, Page & page);
+    friend std::ostream & operator<<(std::ostream & s, Page & page);
 };
 
 //Are these needed here?
-std::istream & operator>>(std::istream & s, std::vector<std::pair<std::string, int> > & data);
-std::ostream & operator<<(std::ostream & s, std::vector<std::pair<std::string, int> > & data);
-
+//std::istream & operator>>(std::istream & s, std::vector<std::pair<std::string, int> > & data);
+//std::ostream & operator<<(std::ostream & s, std::vector<std::pair<std::string, int> > & data);
 
 #endif
