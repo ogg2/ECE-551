aa130de1b191f06db704f1b66bebeb1402260b51
ogg4 <ogg4@duke.edu>
2021-11-15 14:16:11 -0500

    Assignment 103, building tree

--- 103_huff_tree/buildTree.cpp
+++ 103_huff_tree/buildTree.cpp
@@ -2,4 +2,25 @@
 
 Node *  buildTree(uint64_t * counts) {
   //WRITE ME!
+  priority_queue_t pq;
+
+  for (int i = 0; i < 257; i++) {
+    if (counts[i] > 0) {
+      pq.push(new Node(i,counts[i]));
+      //ADD TO PQ
+    }
+  }
+
+  while (pq.size() > 1) {
+    Node * first = pq.top();
+    pq.pop();
+    Node * second = pq.top();
+    pq.pop();
+    Node * combined = new Node (first, second);
+    //delete first;
+    //delete second;
+    pq.push(combined);
+  }
+
+  return pq.top();
 }  
