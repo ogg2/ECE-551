6d9a548615b0351fb620a8db0e2c0a7b1bfe7de9
ogg4 <ogg4@duke.edu>
2021-11-17 22:49:13 -0500

    Fixing user input, floats

--- 093_eval3/book.cpp
+++ 093_eval3/book.cpp
@@ -75,14 +75,14 @@ void Book::readBook () {
       freeBookMemory();
       exit (EXIT_SUCCESS);
     }
-    size_t userChoice;
-    //float userChoice;
+    //size_t userChoice;
+    float userChoice;
     bool invalidInput = true;
 
     while (invalidInput) {
       std::cin >> userChoice;
-      if (std::cin.good() && userChoice <= thisPage->getChoices().size() 
-          && userChoice > 0) {//{ && fmod(userChoice, 1.0) == 0.0) {
+      if (std::cin.good() && (size_t) userChoice <= thisPage->getChoices().size() 
+          && (size_t) userChoice > 0 && fmod(userChoice, 1.0) == 0.0) {
         invalidInput = false;
       } else {
         std::cout << "That is not a valid choice, please try again" << std::endl;
